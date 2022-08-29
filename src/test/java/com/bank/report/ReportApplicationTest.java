99a3e24233744873cba8a642e993f17772266f30
ttitomary <ttitomary@gmail.com>
2022-08-29 01:46:46 -0500

    actions

---
+++ src/test/java/com/bank/report/ReportApplicationTest.java
@@ -0,0 +1,15 @@
+package com.bank.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReportApplicationTest {
+
+    @Test
+    void main() {
+        ReportApplication.main(new String[] {});
+    }
+}
