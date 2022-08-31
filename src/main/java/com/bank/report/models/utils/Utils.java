a7364957910b3c49dd96fc816363766959177234
AdrianNMS <adrian.n.melgarejo.s@gmail.com>
2022-08-31 17:41:21 -0500

    Fixed between dates function

--- src/main/java/com/bank/report/models/utils/Utils.java
+++ src/main/java/com/bank/report/models/utils/Utils.java
@@ -1,6 +1,5 @@
 package com.bank.report.models.utils;
 
-import com.bank.report.controller.ReportRestControllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,27 +11,10 @@ import java.time.temporal.ChronoField;
 public class Utils {
 
     private static final Logger log = LoggerFactory.getLogger(Utils.class);
-    public static boolean Between(int variable, int minValueInclusive, int maxValueInclusive) {
-        return variable >= minValueInclusive && variable <= maxValueInclusive;
-    }
 
     public static boolean BetweenDates(LocalDateTime date, LocalDateTime minDate, LocalDateTime maxDate)
     {
-        log.info("[INI] BetweenDates");
-        log.info("date : " + date);
-        log.info("minDate : " + minDate);
-        log.info("maxDate : " + maxDate);
-        log.info("[END] BetweenDates");
-
-        var year = Between(date.getYear(),minDate.getYear(), maxDate.getYear());
-        var month = Between(date.getMonthValue(),minDate.getMonthValue(),maxDate.getMonthValue());
-        var day = Between(date.getDayOfMonth(),minDate.getDayOfMonth(), maxDate.getDayOfMonth());
-
-        log.info("year : " + year);
-        log.info("month : " + month);
-        log.info("day : " + day);
-
-        return year && month && day;
+        return date.isAfter(minDate.minusDays(1))&& date.isBefore(maxDate.plusDays(1));
     }
 
     public static LocalDateTime parseLocalDate(String date)
